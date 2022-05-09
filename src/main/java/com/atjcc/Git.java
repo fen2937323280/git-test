cbdc390f9bc76bd4b71442ffa4cc8b83ba3cca67
jixiaoji <jixiaoji@qq.com>
2022-05-09 12:11:13 +0800

    change1 push

--- src/main/java/com/atjcc/Git.java
+++ src/main/java/com/atjcc/Git.java
@@ -14,6 +14,6 @@ public class Git {
         System.out.println("master commit1");
         System.out.println("hot-fix commit1");
         System.out.println("push test");
-        System.out.println("pull test")
+        System.out.println("pull test");
     }
 }
